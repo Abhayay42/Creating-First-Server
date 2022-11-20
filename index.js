2ab7080de92f1dbf6c71c64a6a51474c35abb5b1
Abhayay42 <abhayth1441@gmail.com>
2022-11-20 19:39:29 +0530

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const path = require('path')
 const port = 9000;
 const app = express();
+
+app.set('view engine','ejs');
+app.set('dirs',path.join(__dirname,'dirs'))
+
+app.get('/', function(req,res){
+    return res.render('profile',{
+        title:"my profile!"
+    })
+})
 
 app.get('/',function(req,res){
     res.send('<h1> Hello this is my first server</h1>')
    
 })
-app.get('/1',function(req,res){
-    res.send('<h2> never mind this page doesnt looks so good but we will make it look good soon!!</h1>')
-})
-
 
 app.listen(port,function(err){
     if(err){
